b30a484bb8adf093017046e08f941fdb0574321d
sandrine-bd <sandrine.barrucand@le-campus-numerique.fr>
2025-12-05 15:01:15 +0100

    Ajout composant CitiesList

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
-function onListClick() {
-  alert("Vous avez cliqué pour afficher la liste des données météo");
-}
+import CitiesList from './views/CitiesList.vue';
 </script>
 
 <template>
   <main class="home">
-    <h1>Bienvenue sur l'application météo</h1>
-    <a href="#" @click.prevent="onListClick">
-      Voir la liste des données météo
-    </a>
+    <CitiesList />
   </main>
 </template>
 
