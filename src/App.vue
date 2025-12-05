acd7d05baaab1ad99d26f71f5758acc847874d8a
sandrine-bd <sandrine.barrucand@le-campus-numerique.fr>
2025-12-05 14:59:35 +0100

    Création du projet et écran d'accueil

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+function onListClick() {
+  alert("Vous avez cliqué pour afficher la liste des données météo");
+}
+</script>
+
+<template>
+  <main class="home">
+    <h1>Bienvenue sur l'application météo</h1>
+    <a href="#" @click.prevent="onListClick">
+      Voir la liste des données météo
+    </a>
+  </main>
+</template>
+
+<style scoped>
+.home {
+  padding: 2rem;
+}
+a {
+  color: #42b983;
+  cursor: pointer;
+  text-decoration: underline;
+}
+</style>
